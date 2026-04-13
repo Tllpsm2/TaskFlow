81a32233d676cada90d364b4a32efc1690fd507b
Jo Al <1-19-22394@aluno.faminas.edu.br>
2026-04-13 09:31:24 -0300

    feat: adiciona configuração de CORS para permitir requisições do Angular

--- TaskFlow.Api/Program.cs
+++ TaskFlow.Api/Program.cs
@@ -10,20 +10,35 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<TarefaService>();
 
-// DbContext
-// A chave "DefaultConnection" é lida do User Secrets (ou de appsettings.json em produção).
-// Para configurá-la localmente via User Secrets, execute no terminal (dentro de TaskFlow.Api/):
-//   dotnet user-secrets init
-//   dotnet user-secrets set "ConnectionStrings:DefaultConnection" "Server=...;Database=...;User Id=...;Password=...;TrustServerCertificate=true"
-// Se quiser usar um nome diferente de "DefaultConnection", altere tanto aqui quanto na chave do User Secret.
+/* DbContext
+A chave "DefaultConnection" é lida do User Secrets (ou de appsettings.json em produção).
+ Para configurá-la localmente via User Secrets, execute no terminal (dentro de TaskFlow.Api/):
+    > dotnet user-secrets init
+    > dotnet user-secrets set "ConnectionStrings:DefaultConnection" "Server=...;Database=...;User Id=...;Password=...;TrustServerCertificate=true"
+ Se quiser usar um nome diferente de "DefaultConnection", altere tanto aqui quanto na chave do User Secret.
+*/
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(connectionString));
+
+// CORS - Angular
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
+
+app.UseCors("Angular");
 
 app.UseHttpsRedirection();
 
