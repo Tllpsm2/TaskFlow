d36a06c20f4c227d8c3cdbb114b8445e8f27ff33
Jo Al <1-19-22394@aluno.faminas.edu.br>
2026-04-13 16:11:05 -0300

    feat: adiciona tipos e interfaces para gerenciamento de tarefas

--- taskflow-app/src/app/core/models/models.ts
+++ taskflow-app/src/app/core/models/models.ts
@@ -1,2 +1,17 @@
-export interface Models {
+export type StatusTarefa = 'Pendente' | 'EmAndamento' | 'Concluida';
+
+export interface Tarefa {
+    id: number;
+    titulo: string;
+    descricao: string;
+    dataCriacao: Date;
+    dataInicio?: Date;
+    dataConclusao?: Date;
+    status: StatusTarefa;
+}
+
+export interface TarefaRequest{
+    titulo: string;
+    descricao: string;
+    status: StatusTarefa;
 }
