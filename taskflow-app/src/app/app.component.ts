449c8a14c372a031d10c7467eb67b6f2f73d5a28
Jo Al <1-19-22394@aluno.faminas.edu.br>
2026-04-14 10:09:19 -0300

    refactor: separar responsabilidades entre componente e TarefaService

--- taskflow-app/src/app/app.component.ts
+++ taskflow-app/src/app/app.component.ts
@@ -1,7 +1,6 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { Tarefa, TarefaRequest } from './core/models/models';
+import { TarefaService } from './core/tarefa.service';
 
 @Component({
   selector: 'app-root',
@@ -10,32 +9,6 @@ import { Tarefa, TarefaRequest } from './core/models/models';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-
 export class AppComponent {
-  private readonly api = `http://localhost:5055/api/Tarefas`;
-  private http = inject(HttpClient);
-
-  listar(status?: string) {
-    let params = new HttpParams();
-    if (status) {
-      params = params.set('status', status);
-    }
-    return this.http.get<Tarefa[]>(this.api, { params });
-  }
-
-  buscarPorId(id: number) {
-    return this.http.get<Tarefa>(`${this.api}/${id}`);
-  }
-
-  criar(request: TarefaRequest) {
-    return this.http.post<Tarefa>(this.api, request);
-  }
-  
-  atualizar(id: number, request: TarefaRequest) {
-    return this.http.put<Tarefa>(`${this.api}/${id}`, request);
-  }
-
-  excluir(id: number) {
-    return this.http.delete(`${this.api}/${id}`);
-  }
+  private tarefaService = inject(TarefaService);
 }
--- taskflow-app/src/app/core/tarefa.service.ts
+++ taskflow-app/src/app/core/tarefa.service.ts
@@ -1,9 +1,35 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { StatusTarefa, Tarefa, TarefaRequest } from './models/models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TarefaService {
+  private readonly api = `http://localhost:5055/api/Tarefas`;
+  private http = inject(HttpClient);
 
-  constructor() { }
+  listar(status?: StatusTarefa) {
+    let params = new HttpParams();
+    if (status) {
+      params = params.set('status', status);
+    }
+    return this.http.get<Tarefa[]>(this.api, { params });
+  }
+
+  buscarPorId(id: number) {
+    return this.http.get<Tarefa>(`${this.api}/${id}`);
+  }
+
+  criar(request: TarefaRequest) {
+    return this.http.post<Tarefa>(this.api, request);
+  }
+
+  atualizar(id: number, request: TarefaRequest) {
+    return this.http.put<Tarefa>(`${this.api}/${id}`, request);
+  }
+
+  excluir(id: number) {
+    return this.http.delete(`${this.api}/${id}`);
+  }
 }
