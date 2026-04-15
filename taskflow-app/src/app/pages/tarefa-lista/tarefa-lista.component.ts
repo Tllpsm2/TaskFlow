ef03733a14caf91c012c6fb881e96a507e18a641
Jo Al <1-19-22394@aluno.faminas.edu.br>
2026-04-15 19:41:58 -0300

    feat: aprimora componente TarefaLista com tratamento de erros e exibição de mensagens

--- taskflow-app/src/app/pages/tarefa-lista/tarefa-lista.component.ts
+++ taskflow-app/src/app/pages/tarefa-lista/tarefa-lista.component.ts
@@ -1,11 +1,14 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { Tarefa } from '../../core/models/models';
 import { TarefaService } from '../../core/tarefa.service';
+import { RouterLink } from '@angular/router';
+import { DatePipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-tarefa-lista',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, DatePipe],
   templateUrl: './tarefa-lista.component.html',
   styleUrl: './tarefa-lista.component.css'
 })
@@ -16,22 +19,46 @@ export class TarefaListaComponent implements OnInit {
 
   tarefas = signal<Tarefa[]>([]);
   carregando = signal(false);
+  mensagemErro = signal('');
+
+  // divisão por status
+  pendentes = computed(() => this.tarefas().filter(t => t.status === 'Pendente'));
+  emAndamento = computed(() => this.tarefas().filter(t => t.status === 'EmAndamento'));
+  concluidas = computed(() => this.tarefas().filter(t => t.status === 'Concluida'));
 
   ngOnInit() {
     this.carregarTarefas();
   }
 
-  carregarTarefas(){
+  carregarTarefas() {
     this.carregando.set(true);
+    this.mensagemErro.set('');
+
     this.tarefaService.listar()
       .subscribe({
         next: tarefas => {
           this.tarefas.set(tarefas);
           this.carregando.set(false);
         },
+        error: error => {
+          console.error('Erro ao carregar tarefas:', error);
+          this.mensagemErro.set(`Houve algum erro ao carregar as tarefas. Por favor, tente novamente mais tarde. "${error.message}"`);
+          this.carregando.set(false);
+        }
+      });
+  }
+
+  excluirTarefa(tarefa: Tarefa) {
+    if (!confirm(`Excluir a tarefa #${tarefa.id} - "${tarefa.titulo}"?`)) {
+      return;
+    }
+
+    this.tarefaService.excluir(tarefa.id).subscribe({
+      next: () => {
+        this.tarefas.update(tarefas => tarefas.filter(t => t.id !== tarefa.id));
+      },
       error: error => {
-        console.error('Erro ao carregar tarefas:', error);
-        this.carregando.set(false);
+        console.error('Erro ao excluir tarefa:', error);
       }
     });
   }
