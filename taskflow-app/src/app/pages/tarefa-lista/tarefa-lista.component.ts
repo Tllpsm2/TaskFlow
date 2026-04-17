6d5c239abc3f2d788252b99881b7b6e2a3955fb4
Jo Al <1-19-22394@aluno.faminas.edu.br>
2026-04-17 15:25:18 -0300

    feat: adiciona suporte a drag-and-drop na lista de tarefas e atualiza dependências do Angular

--- taskflow-app/src/app/pages/tarefa-lista/tarefa-lista.component.ts
+++ taskflow-app/src/app/pages/tarefa-lista/tarefa-lista.component.ts
@@ -1,15 +1,15 @@
 import { Component, computed, inject, OnInit, signal } from '@angular/core';
-import { Tarefa } from '../../core/models/models';
+import { StatusTarefa, Tarefa, TarefaRequest } from '../../core/models/models';
 import { TarefaService } from '../../core/tarefa.service';
 import { RouterLink } from '@angular/router';
 import { DatePipe } from '@angular/common';
 import { LucideAngularModule, Pencil, Trash2 } from 'lucide-angular';
-
+import { CdkDragDrop, DragDropModule } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-tarefa-lista',
   standalone: true,
-  imports: [RouterLink, DatePipe, LucideAngularModule],
+  imports: [RouterLink, DatePipe, LucideAngularModule, DragDropModule],
   templateUrl: './tarefa-lista.component.html',
   styleUrl: './tarefa-lista.component.css'
 })
@@ -22,6 +22,7 @@ export class TarefaListaComponent implements OnInit {
   carregando = signal(false);
   mensagemErro = signal('');
   
+
   // símbolos para botões
   readonly pencil = Pencil;
   readonly trash2 = Trash2;
@@ -79,6 +80,29 @@ export class TarefaListaComponent implements OnInit {
       },
       error: error => {
         console.error('Erro ao excluir tarefa:', error);
+      }
+    });
+  }
+
+  // implementa drag-drop
+  onDrop(event: CdkDragDrop<Tarefa[]>, novoStatus: StatusTarefa) {
+    if (event.previousContainer === event.container) {
+      return; // arrastar no mesmo container
+    }
+
+    const tarefaArrastada = event.previousContainer.data[event.previousIndex];
+    const request: TarefaRequest = {
+      titulo: tarefaArrastada.titulo,
+      descricao: tarefaArrastada.descricao,
+      status: novoStatus
+    };
+
+    this.tarefaService.atualizar(tarefaArrastada.id, request).subscribe({
+      next: () => {
+        this.carregarTarefas();
+      },
+      error: error => {
+        console.error('Erro ao atualizar status da tarefa:', error);
       }
     });
   }
